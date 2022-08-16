166e7019b9a0e2d4e256696ba919b8c740574ae3
a891021960035 <a891021960035@gmail.com>
2022-08-17 01:25:37 +0800

    correct can frame is_extended from 0 to 1

--- include/state_controller.hpp
+++ include/state_controller.hpp
@@ -84,8 +84,9 @@ public:
   }
   void checkVCU() {
     can_msgs::Frame vcu1msg;
-    vcu1msg.id = 0x0008A7D0;
     vcu1msg.header.stamp = ros::Time::now();
+    vcu1msg.id = 0x0008A7D0;
+    vcu1msg.is_extended = 1;
     vcu1msg.data = { 1, 0, 0, 0, 0, 0, 0, 0 };
     can_pub_.publish(vcu1msg);
     std::cout << "CAN published" << std::endl;
